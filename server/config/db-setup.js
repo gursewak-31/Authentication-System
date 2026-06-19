7cce739a00e045b52abde92a8b887511a7183360
gutsewak singh <gursewak0077sin@gmail.com>
2026-06-19 21:41:41 +0530

    add database setup file

---
+++ server/config/db-setup.js
@@ -0,0 +1,42 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./Authentication-System/server/.env"
+});
+
+async function DBsetup(params) {
+    try{
+        let conn = await mysql.createConnection({
+            host: process.env.HOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD
+        });
+
+        await conn.execute("CREATE DATABASE IF NOT EXISTS authentication_system_db");
+        await conn.execute("USE authentication_system_db");
+
+        await conn.execute(`CREATE IF NOT EXISTS TABLE users (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                firstName VARCHAR(20),
+                                lastName VARCHAR(20),
+                                email VARCHAR(50),
+                                password VARCHAR(20),
+                                profileImage VARCHAR(100),
+                                lastUpdate DATETIME
+        )`);
+
+        await conn.execute(`CREATE IF NOT EXISTS TABLE login_sessions (
+                                sessionId VARCHAR(20) PRIMARY KEY,
+                                email VARCHAR(50),
+                                password VARCHAR(20)
+        )`);               
+
+        console.log("Database setup");
+        await conn.end();
+    }catch(err){
+        console.log("failed to setup database ", err);
+    }
+}
+
+DBsetup();
