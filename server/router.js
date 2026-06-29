9fc67c5cf6eba38b8137aa97c2d38cd3579d6e71
gutsewak singh <gursewak0077sin@gmail.com>
2026-06-29 23:08:48 +0530

    fix project structure

---
+++ server/router.js
@@ -0,0 +1,45 @@
+import * as controller from "./controllers/controller.js"
+
+export default function router(request, response){
+    if(request.method == "OPTIONS"){
+        response.statusCode = 204;
+        response.end();
+        return;
+    }
+
+    if(request.method == "POST" && request.url == "/signup"){
+        return controller.userSignup(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/login"){
+        return controller.userLogin(request, response);
+    }
+
+    if(request.method == "GET" && request.url == "/user"){
+        return controller.getUser(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/updateUser"){
+        return controller.userUpdate(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/updateProfileImage"){
+        return controller.userUpdateImage(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/changePassword"){
+        return controller.userChangePassword(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/logout"){
+        return controller.userLogout(request, response);
+    }
+
+    if(request.method == "POST" && request.url == "/deleteAccount"){
+        return controller.userDelete(request, response);
+    }
+
+    response.statusCode = 500;
+    response.end(JSON.stringify({status: "error", msg: "No Route Found"}));
+    return;
+}
